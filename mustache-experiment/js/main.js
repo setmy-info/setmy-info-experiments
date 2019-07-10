15763e74bce765ecf0487db1e69176f4a0bc6c84
Imre Tabur <imre.tabur@eesti.ee>
2019-07-10 20:45:51 +0300

    Updated example

--- mustache-experiment/js/main.js
+++ mustache-experiment/js/main.js
@@ -9,10 +9,39 @@ var app = (function (app) {
         this.init();
     };
 
+    app.onHashChange = function (event) {
+        var hash = window.location.hash.substring(1);
+        var parsed = {
+            hash: hash,
+            parts: hash.split('/').filter(function (element) {
+                return (!!element);
+            }),
+            event: event
+        };
+        for (var i = 0; i < parsed.parts.length; i++) {
+            var part = parsed.parts[i];
+            // TODO : automatic integer, float, boolean, string
+        }
+        if (app.hashHandler) {
+            app.hashHandler(parsed);
+            app.loadTemplate();
+        }
+    };
+
     app.init = function () {
         console.log("app.init");
         this.mainElement = document.getElementById(this.MAIN_ID);
+        window.onhashchange = this.onHashChange;
         console.log(window.location);
     };
+
+    app.loadTemplate = function () {
+        $.get('template.html', function (template) {
+            var rendered = Mustache.render(template, {name: "Imre"});
+            document.getElementById(app.MAIN_ID).innerHTML = rendered;
+        });
+    };
+
     return app;
+
 })(app || {}); 
--- mustache-experiment/js/router.js
+++ mustache-experiment/js/router.js
@@ -3,6 +3,11 @@ var app = (function (app) {
     app.router = {
         init: function () {
             console.log("app.router.init");
+            app.hashHandler = this.hashHandler;
+        },
+        hashHandler: function (hash) {
+            console.log("Hash change event: ", hash);
+            // TODO : by config find callback function to call
         }
     };
 
