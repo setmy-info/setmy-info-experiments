490e425a4d7aa5b3b5c653366d97c6f72fc449bb
Imre Tabur <imre.tabur@mail.ee>
2025-03-17 08:32:41 +0200

    Relu func

---
+++ ann/src/test/java/info/setmy/ann/functions/ReluFunctionTest.java
@@ -0,0 +1,21 @@
+package info.setmy.ann.functions;
+
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.within;
+import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
+
+class ReluFunctionTest {
+
+    ActivationFunction relu;
+
+    @Test
+    void testReluFunction() {
+        relu = new ReluFunction();
+
+        assertThat(relu.func(-1.0)).isCloseTo(0.0, within(0.0001));
+        assertThat(relu.func(-2.0)).isCloseTo(0.0, within(0.0001));
+        assertThat(relu.func(1.0)).isCloseTo(1.0, within(0.0001));
+        assertThat(relu.func(2.0)).isCloseTo(2.0, within(0.0001));
+    }
+}
